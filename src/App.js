e53e4ac4e39b785e674ada0c9087aafe37009561
Saewon Hwang <saebeezy@192.168.1.104>
2019-05-11 19:50:04 -0700

    Fetching data from Google spreadsheet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import config from './config';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${ config.spreadsheetId }/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${ config.apiKey }`;
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(url).then(response => response.json()).then(data => {
+      console.log(data);
+      let batchRowValues = data.valueRanges[0].values;
+
+      const rows = [];
+      for (let i = 1; i < batchRowValues.length; i++) {
+        let rowObject = {};
+        for (let j = 0; j < batchRowValues[i].length; j++) {
+          rowObject[batchRowValues[0][j]] = batchRowValues[i][j];
+        }
+        rows.push(rowObject);
+      }
+
+      this.setState({ items: rows });
+    });
+}
+
+  render() {
+    console.log("STATE", this.state.items);
+    return (
+      <div className="App">
+  
+      </div>
+    );
+  }
 }
 
 export default App;
